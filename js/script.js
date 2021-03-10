db23e8c8ab35f57e1b5d6f8c066327d48f8c2e30
d0dders <dodders@outlook.com>
2021-03-10 19:45:38 +0000

    Add tshirt options filtering

--- js/script.js
+++ js/script.js
@@ -17,12 +17,20 @@ jobRoleField.addEventListener('change', () => {
 const tshirtDesignField = document.getElementById('design');
 const tshirtColorField = document.getElementById('color');
 const tshirtDesignOptions = document.querySelectorAll('[data-theme]');
-console.log(tshirtDesignOptions);
 tshirtColorField.disabled = true;
 tshirtDesignField.addEventListener('change', () => {
-    if (tshirtDesignField.value = "js puns") {
-
-    } else if (tshirtDesignField.value = "heart js") {
-
+    // Enable color picker and set it to first option
+    tshirtColorField.disabled = false;
+    tshirtColorField.selectedIndex = 0;
+    // Loop the color options and show applicable colors
+    if (tshirtDesignField.value == "js puns" || tshirtDesignField.value == "heart js") {
+        for (let i = 0; i < tshirtDesignOptions.length; i++) {
+            tshirtDesignOptions[i].textContent = tshirtDesignOptions[i].textContent.split('(')[0].trim();
+            if (tshirtDesignOptions[i].getAttribute('data-theme') == tshirtDesignField.value) {
+                tshirtDesignOptions[i].hidden = false;
+            } else {
+                tshirtDesignOptions[i].hidden = true;
+            }
+        }
     }
 });
